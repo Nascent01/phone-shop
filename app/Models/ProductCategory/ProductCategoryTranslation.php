948a503b792c1e9ef2624b288e6c8161f7be6516
Nascent <nasko6670@abv.bg>
2024-10-05 15:39:14 +0300

    Create product category and product category translation models

---
+++ app/Models/ProductCategory/ProductCategoryTranslation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\ProductCategory;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategoryTranslation extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+}

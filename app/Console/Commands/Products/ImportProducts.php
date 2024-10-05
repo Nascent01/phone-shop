2ea2a2bf37eea765c8f963d47d1a4d175386f54c
Nascent <nasko6670@abv.bg>
2024-10-05 22:01:10 +0300

    WIP add logic for inserting products and products translations

--- app/Console/Commands/Products/ImportProducts.php
+++ app/Console/Commands/Products/ImportProducts.php
@@ -1,8 +1,12 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands\Products;
 
+use App\Models\Product\Product;
+use App\Models\Product\ProductTranslation;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class ImportProducts extends Command
@@ -12,14 +16,14 @@ class ImportProducts extends Command
      *
      * @var string
      */
-    protected $signature = 'app:import-products';
+    protected $signature = 'command:import-products';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Import products and categories from json file';
+    protected $description = 'Seed Phone Category and Import products from json file';
 
     /**
      * Execute the console command.
@@ -28,13 +32,41 @@ class ImportProducts extends Command
     {
         $startTime = microtime(true);  
         Log::info('Products import has started');
-            
-        $productsPath = storage_path('Data.json');
-        
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('product_categories')->truncate();
+        DB::table('product_category_translations')->truncate();
+        DB::table('products')->truncate();
+        DB::table('product_translations')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        DB::disableQueryLog();
+
+        Artisan::call('db:seed', ['class' => 'Database\Seeders\ProductCategory\ProductCategoryPhoneSeeder']);
+
+        $productsPath = storage_path('Products.json');
         $products = json_decode(file_get_contents($productsPath), true);
 
+        $locales = config('app.locales');
+
         foreach ($products as $product) {
-           dd($product);
+            $productModelWithoutSymbols = str_replace('_', '', $product['Model']);
+            $productTitle = $product['Brand'] . ' ' . $productModelWithoutSymbols;
+
+           $productnew = Product::insert([
+                'sku' => str()->random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+
+            foreach($locales as $locale) {
+                ProductTranslation::insert([
+                    'product_id' => $productnew,
+                    'locale' => $locale,
+                    'slug' => str_slug($productTitle),
+                    'name' => $productTitle,
+                ]);
+            }
         }
 
         $endTime = microtime(true);   

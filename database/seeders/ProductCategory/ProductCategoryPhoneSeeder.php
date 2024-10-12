0b063aa0be0e40aaf726eef3f7ae86878b68995e
Nascent <nasko6670@abv.bg>
2024-10-12 18:55:54 +0300

    Change ProductCategoryPhoneSeeder using ProductCategoryRepository methods

--- database/seeders/ProductCategory/ProductCategoryPhoneSeeder.php
+++ database/seeders/ProductCategory/ProductCategoryPhoneSeeder.php
@@ -5,26 +5,33 @@ namespace Database\Seeders\ProductCategory;
 use App\Constants\ProductCategory\ProductCategoryConstant;
 use App\Models\ProductCategory\ProductCategory;
 use App\Models\ProductCategory\ProductCategoryTranslation;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Repository\ProductCategory\ProductCategoryRepository;
 use Illuminate\Database\Seeder;
 
 class ProductCategoryPhoneSeeder extends Seeder
 {
+    protected $productCategoryRepository;
+
+    public function __construct(ProductCategoryRepository $productCategoryRepository)
+    {
+        $this->productCategoryRepository = $productCategoryRepository;
+    }
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-       $productCategory = ProductCategory::insert([
+        $locales = config('app.locales');
+
+     $productCategory = $this->productCategoryRepository->create([
             'created_at' => now(),
             'updated_at' => now(),
         ]);
 
-        $locales = config('app.locales');    
-
-        foreach($locales as $locale) {
-            ProductCategoryTranslation::insert([
-                'product_category_id' => $productCategory,
+        foreach ($locales as $locale) {
+            $this->productCategoryRepository->createTranslation([
+                'product_category_id' => $productCategory->id,
                 'locale' => $locale,
                 'name' => ProductCategoryConstant::TYPE_PHONE_CATEGORY_ARRAY[$locale],
             ]);

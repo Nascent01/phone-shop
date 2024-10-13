e8d732a6fbdc04004c3566bb467f8a59a2110a1a
Nascent <nasko6670@abv.bg>
2024-10-13 18:03:43 +0300

    Add logic for processing and inserting products, products translations and products categories

--- app/Console/Commands/Products/ImportProducts.php
+++ app/Console/Commands/Products/ImportProducts.php
@@ -2,12 +2,13 @@
 
 namespace App\Console\Commands\Products;
 
+use App\Constants\ProductCategory\ProductCategoryConstant;
 use App\Models\Product\Product;
 use App\Models\Product\ProductTranslation;
+use App\Models\ProductCategory\ProductCategoryTranslation;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 class ImportProducts extends Command
 {
@@ -30,14 +31,15 @@ class ImportProducts extends Command
      */
     public function handle()
     {
-        $startTime = microtime(true);  
-        Log::info('Products import has started');
+        $startTime = microtime(true);
+        $this->info('Products import has started');
 
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         DB::table('product_categories')->truncate();
         DB::table('product_category_translations')->truncate();
         DB::table('products')->truncate();
         DB::table('product_translations')->truncate();
+        DB::table('product_product_category')->truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         DB::disableQueryLog();
@@ -46,31 +48,79 @@ class ImportProducts extends Command
 
         $productsPath = storage_path('Products.json');
         $products = json_decode(file_get_contents($productsPath), true);
-
         $locales = config('app.locales');
 
-        foreach ($products as $product) {
-            $productModelWithoutSymbols = str_replace('_', '', $product['Model']);
-            $productTitle = $product['Brand'] . ' ' . $productModelWithoutSymbols;
+        $dataToInsertProducts = [];
+        $dataToInsertProductsTranslations = [];
 
-           $productnew = Product::insert([
+        /**
+         * Process and insert products data
+         */
+        foreach ($products as $product) {
+            $dataToInsertProducts[] = [
+                'old_id' => $product['objectId'],
                 'sku' => str()->random(10),
                 'created_at' => now(),
                 'updated_at' => now(),
-            ]);
+            ];
+        }
 
-            foreach($locales as $locale) {
-                ProductTranslation::insert([
-                    'product_id' => $productnew,
-                    'locale' => $locale,
-                    'slug' => str_slug($productTitle),
-                    'name' => $productTitle,
-                ]);
+        if (!empty($dataToInsertProducts) && count($dataToInsertProducts) > 0) {
+            Product::insert($dataToInsertProducts);
+        }
+
+        /**
+         * Process and insert products translations data
+         */
+
+        $productIdsMappedByOldIds = Product::select('id', 'old_id')
+            ->pluck('id', 'old_id')
+            ->toArray();
+
+        foreach ($products as $product) {
+            if ($productIdsMappedByOldIds[$product['objectId']]) {
+                if (!empty($product['Model'])) {
+                    $productModelWithoutSymbols = str_replace('_', '', $product['Model']);
+                    $productTitle = $product['Brand'] . ' ' . $productModelWithoutSymbols;
+                    foreach ($locales as $locale) {
+                        $dataToInsertProductsTranslations[] = [
+                            'product_id' => $productIdsMappedByOldIds[$product['objectId']],
+                            'locale' => $locale,
+                            'slug' => str_slug($productTitle),
+                            'name' => $productTitle,
+                        ];
+                    }
+                }
             }
         }
 
-        $endTime = microtime(true);   
+        $chunkedProductTranslationsData = array_chunk($dataToInsertProductsTranslations, 1500);
+
+        foreach ($chunkedProductTranslationsData as $chunk) {
+            ProductTranslation::insert($chunk);
+        }
+
+        /**
+         * Process and insert products category data
+         */
+
+        $productRecords = Product::all();
+        $productCategory = ProductCategoryTranslation::where('name', ProductCategoryConstant::TYPE_PHONE_CATEGORY_BG)->first();
+        $productProductCategoryData = [];
+
+        foreach ($productRecords as $productRecord) {
+            $productProductCategoryData[] = [
+                'product_id' => $productRecord->id,
+                'product_category_id' => $productCategory->product_category_id,
+            ];
+        }
+
+        if (!empty($productProductCategoryData) && count($productProductCategoryData) > 0) {
+            DB::table('product_product_category')->insert($productProductCategoryData);
+        }
+
+        $endTime = microtime(true);
         $executionTime = $endTime - $startTime;
-        Log::info('Products import task has finished for' . $executionTime);
+        $this->info('Products import task has finished for: ' . $executionTime);
     }
 }

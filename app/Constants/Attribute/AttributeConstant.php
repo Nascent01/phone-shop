42072af572368b95cffb78082292edae9302be02
Nascent <nasko6670@abv.bg>
2024-10-19 16:34:43 +0300

    Create attribute constant with all the attributes from Products.json

---
+++ app/Constants/Attribute/AttributeConstant.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Constants\Attribute;
+
+class AttributeConstant
+{
+    public const TYPE_ATTRIBUTE_MODEL = 'Model';
+    public const TYPE_ATTRIBUTE_BRAND = 'Brand';
+    public const TYPE_ATTRIBUTE_NETWORK = 'Network';
+    public const TYPE_ATTRIBUTE_TWOG = 'TwoG';
+    public const TYPE_ATTRIBUTE_THREEG = 'ThreeG';
+    public const TYPE_ATTRIBUTE_NETWORK_SPEED = 'Network_Speed';
+    public const TYPE_ATTRIBUTE_GPRS = 'GPRS';
+    public const TYPE_ATTRIBUTE_EDGE = 'EDGE';
+    public const TYPE_ATTRIBUTE_STATUS = 'Status';
+    public const TYPE_ATTRIBUTE_DIMENSIONS = 'Dimensions';
+    public const TYPE_ATTRIBUTE_SIM = 'SIM';
+    public const TYPE_ATTRIBUTE_DISPLAY_TYPE = 'Display_type';
+    public const TYPE_ATTRIBUTE_DISPLAY_RESOLUTION = 'Display_resolution';
+    public const TYPE_ATTRIBUTE_DISPLAY_SIZE = 'Display_size';
+    public const TYPE_ATTRIBUTE_OPERATING_SYSTEM = 'Operating_System';
+    public const TYPE_ATTRIBUTE_CPU = 'CPU';
+    public const TYPE_ATTRIBUTE_MEMORY_CARD = 'Memory_card';
+    public const TYPE_ATTRIBUTE_INTERNAL_MEMORY = 'Internal_memory';
+    public const TYPE_ATTRIBUTE_RAM = 'RAM';
+    public const TYPE_ATTRIBUTE_PRIMARY_CAMERA = 'Primary_camera';
+    public const TYPE_ATTRIBUTE_SECONDARY_CAMERA = 'Secondary_camera';
+    public const TYPE_ATTRIBUTE_LOUD_SPEAKER = 'Loud_speaker';
+    public const TYPE_ATTRIBUTE_AUDIO_JACK = 'Audio_jack';
+    public const TYPE_ATTRIBUTE_WLAN = 'WLAN';
+    public const TYPE_ATTRIBUTE_BLUETOOTH = 'Bluetooth';
+    public const TYPE_ATTRIBUTE_GPS = 'GPS';
+    public const TYPE_ATTRIBUTE_RADIO = 'Radio';
+    public const TYPE_ATTRIBUTE_USB = 'USB';
+    public const TYPE_ATTRIBUTE_SENSORS = 'Sensors';
+    public const TYPE_ATTRIBUTE_BATTERY = 'Battery';
+    public const TYPE_ATTRIBUTE_COLORS = 'Colors';
+
+    public const ATTRIBUTES_ARRAY = [
+        self::TYPE_ATTRIBUTE_MODEL,
+        self::TYPE_ATTRIBUTE_BRAND,
+        self::TYPE_ATTRIBUTE_NETWORK,
+        self::TYPE_ATTRIBUTE_TWOG,
+        self::TYPE_ATTRIBUTE_THREEG,
+        self::TYPE_ATTRIBUTE_NETWORK_SPEED,
+        self::TYPE_ATTRIBUTE_GPRS,
+        self::TYPE_ATTRIBUTE_EDGE,
+        self::TYPE_ATTRIBUTE_STATUS,
+        self::TYPE_ATTRIBUTE_DIMENSIONS,
+        self::TYPE_ATTRIBUTE_SIM,
+        self::TYPE_ATTRIBUTE_DISPLAY_TYPE,
+        self::TYPE_ATTRIBUTE_DISPLAY_RESOLUTION,
+        self::TYPE_ATTRIBUTE_DISPLAY_SIZE,
+        self::TYPE_ATTRIBUTE_OPERATING_SYSTEM,
+        self::TYPE_ATTRIBUTE_CPU,
+        self::TYPE_ATTRIBUTE_MEMORY_CARD,
+        self::TYPE_ATTRIBUTE_INTERNAL_MEMORY,
+        self::TYPE_ATTRIBUTE_RAM,
+        self::TYPE_ATTRIBUTE_PRIMARY_CAMERA,
+        self::TYPE_ATTRIBUTE_SECONDARY_CAMERA,
+        self::TYPE_ATTRIBUTE_LOUD_SPEAKER,
+        self::TYPE_ATTRIBUTE_AUDIO_JACK,
+        self::TYPE_ATTRIBUTE_WLAN,
+        self::TYPE_ATTRIBUTE_BLUETOOTH,
+        self::TYPE_ATTRIBUTE_GPS,
+        self::TYPE_ATTRIBUTE_RADIO,
+        self::TYPE_ATTRIBUTE_USB,
+        self::TYPE_ATTRIBUTE_SENSORS,
+        self::TYPE_ATTRIBUTE_BATTERY,
+        self::TYPE_ATTRIBUTE_COLORS,
+    ];
+}

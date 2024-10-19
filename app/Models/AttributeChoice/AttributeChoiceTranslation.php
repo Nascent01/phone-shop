d13d5090dd520d528f2cc7ca880bfe2e36a06995
Nascent <nasko6670@abv.bg>
2024-10-19 22:50:32 +0300

    Create AttributeChoice and AttributeChoiceTranslation models

---
+++ app/Models/AttributeChoice/AttributeChoiceTranslation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\AttributeChoice;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AttributeChoiceTranslation extends Model
+{
+    use HasFactory;
+}

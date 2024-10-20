3ad8f3134a1b51479386b8c5dcb7a9bc4ddcb9dd
Nascent <nasko6670@abv.bg>
2024-10-20 13:18:06 +0300

    Remove unused AttributeChoiceRepository logic

--- app/Models/AttributeChoice/AttributeChoice.php
+++ app/Models/AttributeChoice/AttributeChoice.php
@@ -2,10 +2,18 @@
 
 namespace App\Models\AttributeChoice;
 
+use App\Models\Attribute\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AttributeChoice extends Model
 {
     use HasFactory;
+
+    public $guarded = [];
+
+    public function attribute()
+    {
+        return $this->belongsTo(Attribute::class);
+    }
 }

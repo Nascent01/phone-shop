11d12394be9d997e4d769d17cae2a1db96ce79c0
Nascent <nasko6670@abv.bg>
2024-10-20 13:19:00 +0300

    Add machine name to attribute_choices migration

--- database/migrations/2024_10_19_191231_create_attribute_choices_table.php
+++ database/migrations/2024_10_19_191231_create_attribute_choices_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('attribute_choices', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('attribute_id');
+            $table->string('machine_name');
             $table->timestamps();
 
             $table->foreign('attribute_id')
